3b3ee6dbb7be76dcff1a5fe5140d252c61eba5c1
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-03 00:44:55 +0300

    Create basic layout

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,36 @@
+import { Outlet } from 'react-router-dom';
+import { Layout, Space } from 'antd';
+import SideMenu from '../components/SideMenu/SideMenu.jsx';
+import Navbar from '../components/Navbar/Navbar.jsx';
+
+import styles from './AppLayout.module.css';
+
+const { Header, Sider, Content } = Layout;
+
+function AppLayout() {
+    return (
+        <Space
+            direction="vertical"
+            style={{
+                width: '100%',
+            }}
+            size={[0, 48]}
+        >
+            <Layout>
+                <Sider style={styles.appSider}>
+                    <SideMenu />
+                </Sider>
+                <Layout>
+                    <Header style={styles.appHeader}>
+                        <Navbar />
+                    </Header>
+                    <Content style={styles.appContent}>
+                        <Outlet />
+                    </Content>
+                </Layout>
+            </Layout>
+        </Space>
+    );
+}
+
+export default AppLayout;
