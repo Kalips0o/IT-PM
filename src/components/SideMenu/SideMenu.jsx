b7cdb2d3b66ebbf8b0481d5f9171e63ed845e66a
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-02 19:47:53 +0300

    Correct naming, import Side Menu styles

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import UserAvatar from './UserAvatar';
-import UserInfo from './UserInfo';
+import UserAvatar from './UserAvatar.jsx';
+import UserInfo from './UserInfo.jsx';
+
+import styles from './SideMenu.module.css';
 
 function SideMenu() {
     return (
-        <div className="block">
+        <div className={styles.sideMenu}>
             <UserAvatar />
             <UserInfo />
             <div>Chat</div>
