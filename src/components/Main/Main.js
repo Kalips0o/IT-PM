c2caefc24c2e11385ee7eaa6d9bb99f37afa9dc6
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-02 19:50:38 +0300

    Rename main content component, add module.css file

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import styles from './Main.module.css';
+
+function Main() {
+    return (
+        <div className={styles.content}>
+            <div>1 block</div>
+            <div>2 block</div>
+        </div>
+    );
+}
+
+export default Main;
