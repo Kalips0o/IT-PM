75839f15a8c57ab68392ad83a01fd4059eda53b6
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-03 00:45:13 +0300

    Correct indents

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,14 @@
 export default function MainPage() {
-  return (
-    <div style={{ border: '1px black solid' }}>
-      <h2>SideMenu</h2>
-      <div>Avatar</div>
-      <div>Hello, Arina</div>
-      <ul>
-        <li>first btn</li>
-        <li>second btn</li>
-        <li>...other btns</li>
-      </ul>
-    </div>
-  );
+    return (
+        <div style={{ border: '1px black solid' }}>
+            <h2>SideMenu</h2>
+            <div>Avatar</div>
+            <div>Hello, Arina</div>
+            <ul>
+                <li>first btn</li>
+                <li>second btn</li>
+                <li>...other btns</li>
+            </ul>
+        </div>
+    );
 }
