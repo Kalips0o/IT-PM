841a945a070fbe0b8bdaf7e1dba5ad04ca0f6f93
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-02 19:46:24 +0300

    Remove antd icons import and usage

--- src/components/SideMenu/UserAvatar.jsx
+++ src/components/SideMenu/UserAvatar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Space } from 'antd';
 
 function UserAvatar() {
     return (
         <Space direction="vertical" size={90}>
             <Space wrap size={90}>
-                <Avatar size={90} src="https://cs14.pikabu.ru/post_img/2023/02/13/8/1676295806139337963.jpg" icon={<UserOutlined />} />
+                <Avatar size={90} src="https://cs14.pikabu.ru/post_img/2023/02/13/8/1676295806139337963.jpg" />
             </Space>
         </Space>
     );
