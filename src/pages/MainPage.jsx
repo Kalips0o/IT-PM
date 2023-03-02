18a346ebf0d91d9e5463c40459eebc90abf6a707
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-03 00:49:08 +0300

    Remove odd content

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,7 @@
 export default function MainPage() {
     return (
-        <div style={{ border: '1px black solid' }}>
-            <h2>SideMenu</h2>
-            <div>Avatar</div>
-            <div>Hello, Arina</div>
-            <ul>
-                <li>first btn</li>
-                <li>second btn</li>
-                <li>...other btns</li>
-            </ul>
+        <div>
+            Here will be cards with connected chats
         </div>
     );
 }
