d637212ff802257e86df33161fa450299b86bfb3
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-10 18:08:55 +0300

    Remove unnecessary Space component

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { Layout, Space } from 'antd';
+import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import SideMenu from '../components/SideMenu/SideMenu.jsx';
 import Navbar from '../components/Navbar/Navbar.jsx';
@@ -10,27 +10,19 @@ const { Header, Content, Sider } = Layout;
 
 function AppLayout() {
     return (
-        <Space
-            direction="vertical"
-            style={{
-                width: '100%',
-            }}
-            size={[0, 48]}
-        >
-            <Layout className={styles.layout}>
-                <Sider width={292} style={appSider}>
-                    <SideMenu />
-                </Sider>
-                <Layout>
-                    <Header className={styles.appHeader}>
-                        <Navbar />
-                    </Header>
-                    <Content className={styles.appContent}>
-                        <Outlet />
-                    </Content>
-                </Layout>
+        <Layout className={styles.layout}>
+            <Sider width={292} style={appSider}>
+                <SideMenu />
+            </Sider>
+            <Layout>
+                <Header className={styles.appHeader}>
+                    <Navbar />
+                </Header>
+                <Content className={styles.appContent}>
+                    <Outlet />
+                </Content>
             </Layout>
-        </Space>
+        </Layout>
     );
 }
 
