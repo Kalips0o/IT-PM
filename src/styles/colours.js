7cca9795e71752cf4ea95ec06ab3033dd2bcda2e
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-03-12 00:12:19 +0300

    Divide colours from vars.scss into partial files

---
+++ src/styles/colours.js
@@ -0,0 +1,25 @@
+// KEEP WATCH! This file is based on '_vars.scss' and must be synced with it
+
+export default {
+    // Basic colours
+    white: '#FFFFFF',
+    bg: '#F4F6F9',
+    cardStroke: '#E0E3E7',
+    gray: '#B8BBC6',
+    gray2: '#B6C9D4',
+    secondText: '#3E3F4B',
+    primaryText: '#252525',
+
+    // Accent colours
+    darkBlue: '#4D90F1',
+    blue: '#7AB0FF',
+    selected: '#C0DFFC',
+    purpleLight: '#D6CEF9',
+    purple: '#B3C8F2',
+
+    // Functional colours
+    red: '#D36668',
+    green: '#60DA74',
+    greenLight: '#CEF9D5',
+    yellow: '#FFEE94',
+};
